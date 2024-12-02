b5970622dcea54c35cfbd36ede8248bd13dfec76
Tikeswar Seth <90958826+xCodeFactor@users.noreply.github.com>
2024-12-03 01:23:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
-import Editor from "./components/Editor";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Editor from './components/Editor';
+import io from 'socket.io-client';
+import './App.css';
+
+const socket = io('http://localhost:8080'); // Replace with your Collaboration Service URL
 
 const App = () => {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    socket.on('code-change', (newCode) => {
+      setCode(newCode);
+    });
+
+    return () => {
+      socket.off('code-change');
+    };
+  }, []);
 
   const handleCodeChange = (newCode) => {
     setCode(newCode);
-    // Send code changes to the Collaboration Service
+    socket.emit('code-change', newCode);
   };
 
   return (

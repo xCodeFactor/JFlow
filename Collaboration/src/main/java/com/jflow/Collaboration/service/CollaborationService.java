126946fce21b3560135bc5a2dfebaddc111ac296
Tikeswar Seth <tikeswar.seth@ad.infosys.com>
2024-12-03 00:41:57 +0530

    Error fixed for CollaborationService bean Injection

--- Collaboration/src/main/java/com/jflow/Collaboration/service/CollaborationService.java
+++ Collaboration/src/main/java/com/jflow/Collaboration/service/CollaborationService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Service
 public class CollaborationService {
     @Autowired
     private CodeChangeRepository codeChangeRepository;
